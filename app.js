b73fa7ea3bbd7d83ae3ba7b30599f9c983c1538a
Akash <akashrajpoots14@gmail.com>
2021-03-01 17:47:16 -1200

    updated with MysQl

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressHbs = require('express-handlebars');
 const errorController = require('./controllers/error');
+const db = require('./util/database');
+
+
 
 const app = express();
 
@@ -11,7 +14,7 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views', 'views');
 
-const adminRoutes= require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
