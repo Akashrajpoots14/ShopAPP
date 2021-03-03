edacd6f72ed3fcaede35435b34f468f0567c7a0e
Akash <akashrajpoots14@gmail.com>
2021-03-02 22:57:59 -1200

    Update before mongo Server

--- app.js
+++ app.js
@@ -14,27 +14,28 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
-app.use(errorController.get404);   
+ app.use(errorController.get404);   
 
-sequelize.sync().then(result =>{
-    console.log(result);
-    app.listen(3000);
-})
-.catch(err =>{
-    console.log(err);
-});
+// sequelize.sync().then(result =>{
+//     console.log(result);
+//     app.listen(3000);
+// })
+// .catch(err =>{
+//     console.log(err);
+// });
 
-mongoConnect(client =>{
+mongoConnect((client) => {
+    
     console.log(client);
     app.listen(3000);
+    
 });
-
--- util/database.js
+++ util/database.js
@@ -1,11 +1,11 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-
-
 const mongoConnect = (callback) =>{
  
-MongoClient.connect('mongodb+srv://new-user:Compiler_14@cluster0.mjmwl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+//MongoClient.connect('mongodb+srv://new-user:Compiler_14@cluster0.mjmwl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+MongoClient.connect('mongodb+srv://new-user:Compiler_14@cluster0.mjmwl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 .then( client =>{
   console.log('Connected!');
   callback(client);
